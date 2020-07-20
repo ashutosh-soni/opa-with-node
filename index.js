b49d794dd64aee55b5c9db7a95b1b139c546029e
Ashutosh Soni <viknat1994@gmail.com>
2020-07-21 00:49:57 +0530

    added opa as a child prcoess

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require("express");
+const child_process = require("child_process");
 const bodyParser = require("body-parser");
+const colors = require("colors");
+
 const config = require("./config/config").config;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const startOpaServer = () => {
+  var workerProcess = child_process.spawn("./opa", ["run", "--server"]);
+
+  workerProcess.stdout.on("data", function (data) {
+    console.log("[LOG][stdout:][OPA]: ".brightMagenta + data);
+  });
+
+  workerProcess.stderr.on("data", function (data) {
+    console.log("[LOG][stderr:][OPA]: ".brightMagenta + data);
+  });
+
+  workerProcess.on("close", function (code) {
+    console.log("child process exited with code :".brightMagenta + code);
+  });
+};
+
 const boot = () => {
   console.log("config", config);
   const port = config["port"];
+  startOpaServer();
   app.listen(port, () => {
-    console.log("App started at port:", port);
+    console.log("App started at port:".green.bold, port);
   });
 };
 
